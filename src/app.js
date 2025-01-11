116eddca05c254396b7f8afc7c2eb68d3a452e4b
devnouiq <dev@nouiq.com>
2025-01-11 16:42:40 +0530

    comment out welcome message route in app.js

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ app.use("/api", statsRoute);
 app.use("/api", deviationRoute);
 
 app.use((req, res) => res.status(404).json({ error: "Route not found" }));
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to Crypto API" });
-}
-);
+// app.get("/", (req, res) => {
+//     res.json({ message: "Welcome to Crypto API" });
+// }
+// );
 
 module.exports = app;
